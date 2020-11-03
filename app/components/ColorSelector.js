f44ed5156a020a6899fa98332d84e70d2188c4a6
MhrjAniiz <anishmaharjan17@gmail.com>
2020-11-03 08:32:00 +0545

    added custom color picker

--- app/components/ColorSelector.js
+++ app/components/ColorSelector.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
 
-export default function ColorSelector({ title, Fcolor, Scolor }) {
+export default function ColorSelector({
+  title,
+  Fcolor,
+  Scolor,
+  onIncrease,
+  onDecrease,
+}) {
   return (
     <View>
       <Text>{title}</Text>
-      <Button color={Fcolor} title={`more ${title}`} />
-      <Button color={Scolor} title={`less ${title}`} />
+      <Button
+        onPress={() => onIncrease()}
+        color={Fcolor}
+        title={`more ${title}`}
+      />
+      <Button
+        onPress={() => onDecrease()}
+        color={Scolor}
+        title={`less ${title}`}
+      />
     </View>
   );
 }
--- app/screens/CustomColor.js
+++ app/screens/CustomColor.js
@@ -6,7 +6,30 @@ import { useState } from "react";
 export default function CustomColor() {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
-  const [Blue, setBlue] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  const COLOR_INCREMENT = 15;
+
+  const setColor = (color, change) => {
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+      default:
+        return;
+    }
+  };
+
   return (
     <View
       style={{
@@ -19,16 +42,33 @@ export default function CustomColor() {
           width: "80%",
         }}
       >
-        <ColorSelector title="red" Fcolor="red" Scolor="tomato" />
-        <ColorSelector title="green" Fcolor="green" Scolor="lightgreen" />
-        <ColorSelector title="blue" Fcolor="blue" />
+        <ColorSelector
+          title="red"
+          onIncrease={() => setColor("red", COLOR_INCREMENT)}
+          onDecrease={() => setColor("red", -1 * COLOR_INCREMENT)}
+          Fcolor="red"
+          Scolor="tomato"
+        />
+        <ColorSelector
+          title="green"
+          Fcolor="green"
+          Scolor="lightgreen"
+          onIncrease={() => setColor("green", COLOR_INCREMENT)}
+          onDecrease={() => setColor("green", -1 * COLOR_INCREMENT)}
+        />
+        <ColorSelector
+          title="blue"
+          Fcolor="blue"
+          onIncrease={() => setColor("blue", COLOR_INCREMENT)}
+          onDecrease={() => setColor("blue", -1 * COLOR_INCREMENT)}
+        />
       </View>
       <View
         style={{
           marginTop: 70,
-          width: 100,
-          height: 100,
-          backgroundColor: "red",
+          width: 200,
+          height: 200,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`,
         }}
       ></View>
     </View>
